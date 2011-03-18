59e7ccf53512a1137e3c4b666d23b249fd06a699
Jeff Ling <jeff.ummu@gmail.com>
2011-03-18 11:39:23 -0700

    Added basic video searching (good enough for now)

--- queue.js
+++ queue.js
@@ -1,10 +1,17 @@
+// Callback for the search form. 
 var searchCB = function(response) {
     var html = "";
     	for(vid in response.videos){
     		var video = response.videos[vid];
-    		html += "Video : " + video.title;
+    		html += "<p><div class=\"videoThumb\">";
+    		html += "<a href=\"http://www.youtube.com/watch?v=" +  video.videoId + "\">";
+    		html += "<img src=\"http://img.youtube.com/vi/" + video.videoId + "/3.jpg\"></a>";
+    		html += "</div>";
+    		html += "<div class=\"videoTitle\">";
+    		html += "<a href=\"http://www.youtube.com/watch?v=" +  video.videoId + "\">" + video.title + "</a>";
+    		html += "</div></p>";
     	}
-    alert(html);
+    $("#searchResults").html(html);
 }
 
 $(document).ready(function() {
