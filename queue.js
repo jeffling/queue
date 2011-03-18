498b060dc82d38ae6c6177b8d4f107bd9011bd0f
Jeff Ling <jeff.ummu@gmail.com>
2011-03-18 13:24:42 -0700

    Added click binding

--- queue.js
+++ queue.js
@@ -1,3 +1,6 @@
+// Globals
+var queue = [];
+
 // Callback for the search form. 
 var searchCB = function(response) {
     var html = "";
@@ -12,12 +15,34 @@ var searchCB = function(response) {
     		html += "</div></p>";
     	}
     $("#searchResults").html(html);
+    setClickBind();
+}  
+
+// Put to the back of the queue
+var addToQueue = function(vid) {
+    queue.push(vid);
+}
+
+// set click event binding
+var setClickBind = function() {
+    // intercept all link clicks to recognize clicks to videos
+
+    $("a").click( function(event) {
+        var url = $(this).attr('href');
+        var results = url.match("[\\?&]v=([^&#]*)");
+        
+        if (results != null) {        
+            event.preventDefault();
+            addToQueue(results[1]); // results[1] is the video ID
+        }
+    });
 }
 
 $(document).ready(function() {
-    $('#searchBox').submit(function() {
-      jQTubeUtil.search($("#textbox").val(), searchCB);
-      return false;
+    setClickBind();
+    // $("#player").tubeplayer("player");
+    $('#searchBox').submit(function(event) {
+        jQTubeUtil.search($("#textbox").val(), searchCB);
+        event.preventDefault();
     });
-    
 })
