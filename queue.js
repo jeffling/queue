b4f36c329c3a58d45ee1982174575846dd318bc2
ummu <jeff.ummu@gmail.com>
2011-03-22 14:46:28 -0400

    Added player, basic queuing

--- queue.js
+++ queue.js
@@ -1,27 +1,28 @@
 // Globals
 var queue = [];
 var currentlyPlaying = false;
+var youtubeplayer;
 
 // Play whatever's on queue
-var playNext = function() { 
-    var next = queue.pop();
-    alert(next);
-    // Example code from library... Should figure out how I want this to be
-    jQuery("#player").tubeplayer({
-    	width: 600, // the width of the player
-    	height: 450, // the height of the player
-    	allowFullScreen: "true", // true by default, allow user to go full screen
-    	initialVideo: next, // the video that is loaded into the player
-    	playerID: "youtube-player", // the ID of the embedded youtube player
-    	preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
-    	onPlay: function(id){currentlyPlaying = play;}, // after the play method is called
-    	onPause: function(){currentlyPlaying = false;}, // after the pause method is called
-    	onStop: function(){currentlyPlaying = false;}, // after the player is stopped
-    	onSeek: function(time){}, // after the video has been seeked to a defined point
-    	onMute: function(){}, // after the player is muted
-    	onUnMute: function(){} // after the player is unmuted
-    });
+var playNext = function() {
+	if (currentlyPlaying) {
+		togglePlay();
+	}
+	
+    var next = popQueue();
+    if (!next) {
+    	return false;
     }
+    
+    jQuery("#player").tubeplayer("play", next);
+};
+
+var togglePlay = function() {
+	if (currentlyPlaying)
+		jQuery("#player").tubeplayer("stop");
+	else
+	    jQuery("#player").tubeplayer("play");
+};
 
 // Callback for the search form. 
 var searchCB = function(response) {
@@ -38,15 +39,39 @@ var searchCB = function(response) {
     	}
     $("#searchResults").html(html);
     setClickBind();
-}
+};
+
+// Takes from the front of the queue
+var popQueue = function() {
+	if (queue.length > 0) {
+		var next = queue.pop();
+	    updateQueue();
+	    return next;
+	}
+	return false;
+};
 
 // Put to the back of the queue
 var addToQueue = function(vid) {
     queue.push(vid);
-    if (queue.length == 1) {
+    
+    // if there is nothing playing, why not play the new video?
+    if (!currentlyPlaying) {
        playNext();
-    } 
-}
+    }
+    updateQueue();
+};
+
+// Update Queue List
+var updateQueue = function() {
+    // update queue list on UI
+    var html = "<ul>";
+    for (vid in queue) {
+    	html += "<li>" + queue[vid] + "</li>";
+    }
+    html += "</ul>";
+    $("#queue-display").html(html);
+};
 
 // set click event binding
 var setClickBind = function() {
@@ -61,13 +86,38 @@ var setClickBind = function() {
             addToQueue(results[1]); // results[1] is the video ID
         }
     });
-}
+};
+
+// Video Stop Handler
+var onStopCB = function() {
+	if (queue.length < 0) {
+		playNext();
+	}
+	else
+		currentlyPlaying = false;
+};
 
 $(document).ready(function() {
+	// set up player
+    jQuery("#player").tubeplayer({
+    	playerID: "youtube-player", // the ID of the embedded youtube player
+    	preferredQuality: "default",// preferred quality: default, small, medium, large, hd720
+    	onPlay: function(id){currentlyPlaying = true;}, // after the play method is called
+    	onPause: function(){currentlyPlaying = false;}, // after the pause method is called
+    	onStop: function(){currentlyPlaying = false;}, // after the player is stopped
+    	onSeek: function(time){}, // after the video has been seeked to a defined point
+    	onMute: function(){}, // after the player is muted
+    	onUnMute: function(){}, // after the player is unmuted
+    	onPlayerEnded: function(){onStopCB();}
+    });
+    
+    // get the player reference just in case
+    youtubeplayer = jQuery("#player").tubeplayer("player");
+    
     setClickBind();
-    // $("#player").tubeplayer("player");
+    
     $('#searchBox').submit(function(event) {
         jQTubeUtil.search($("#textbox").val(), searchCB);
         event.preventDefault();
     });
-})
+});
